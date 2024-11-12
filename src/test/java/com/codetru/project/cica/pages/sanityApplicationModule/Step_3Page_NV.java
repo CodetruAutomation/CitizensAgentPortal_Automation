ef7efbfb5ea87ae51d1d3509e6bbd7ff2d1d9e73
fasiash <fasiuddin@codetru.com>
2024-11-12 17:01:17 +0530

    updated step 3 locators

--- src/test/java/com/codetru/project/cica/pages/sanityApplicationModule/Step_3Page_NV.java
+++ src/test/java/com/codetru/project/cica/pages/sanityApplicationModule/Step_3Page_NV.java
@@ -14,12 +14,12 @@ public class Step_3Page_NV extends CommonPageCICA {
 	private By step3_No = By.xpath("//h3[.='Policy Owner Information']/parent::div/descendant::ion-radio-group/ion-row/ion-col//ion-label[.='No']/following-sibling::ion-radio");
 	private By policyPayor_No = By.xpath("//h3[.='Policy Payor Information']/parent::div/descendant::ion-label[.='No']/following-sibling::ion-radio");
 	private By secondaryAddresseeDesg_No = By.xpath("//h3[.='Secondary Addressee Designation']/parent::div/descendant::ion-label[.='No']/following-sibling::ion-radio");
-	private By step_1 = By.xpath("//span[.='Step 1: Product Information']");
+	private By step1 = By.xpath("//span[.='Step 1: Product Information']");
 	private By calculate = By.xpath("(//div/ion-row/ion-col/ion-button[starts-with(@class, md)])[2]");
 	private By step_1_NextB = By.xpath("//p[.='Is Automatic Premium Loan Desired?']/parent::div/parent::ion-col/parent::ion-row/following::ion-row/ion-col[@size='24' and @size-md='6']/ion-button");
 	private By TableOfContent = By.xpath("//h3[.='Policy Owner Information']/parent::div/following-sibling::ion-row[@style='padding-left: 50px;']/ion-col[@size-md='18']/ion-button[.='Table of Contents']");
 	private By impNotice_Popup = By.xpath("//ion-backdrop/following-sibling::div/div/following-sibling::div/button");
-	private By NextStep2 = By.xpath("//h3[.='Contact Information']/parent::div/following-sibling::ion-row/ion-col[@size='24' and @size-md='6']/ion-button");
+	private By step_2_NextB = By.xpath("//h3[.='Contact Information']/parent::div/following-sibling::ion-row/ion-col[@size='24' and @size-md='6']/ion-button");
 	
 	public void Policy_Owner_Information() {
 
@@ -29,35 +29,38 @@ public class Step_3Page_NV extends CommonPageCICA {
 		WebUI.moveToElement(TableOfContent);
 		WebUI.clickElement(TableOfContent);
 		WebUI.sleep(1);
-		WebUI.clickElementWithJs(step_1);
+		WebUI.clickElementWithJs(step1);
 		WebUI.sleep(1.5);
-		// For Notice Popup
 		try {
 			DriverManager.getDriver().findElement(impNotice_Popup).isDisplayed();
 			WebUI.sleep(0.5);
 			WebUI.clickElement(impNotice_Popup);
- 
-		} catch (Exception ex) {
-			System.out.println("Notice Popup Did Not Appear. " + ex.getMessage());
+			WebUI.sleep(0.5);
+		}catch(Exception exe)
+		{
+			System.out.println("Move to Step-1 page failed. "+exe.getMessage());
 		}
- 
-		WebUI.sleep(0.5);
-		WebUI.scrollToElementAtBottom(calculate);
-		WebUI.sleep(0.2);
-		WebUI.clickElement(calculate);
-		WebUI.sleep(0.2);
- 
+		try {
+			DriverManager.getDriver().findElement(calculate).isDisplayed();
+			WebUI.scrollToElementAtBottom(calculate);
+			WebUI.sleep(0.4);
+			WebUI.clickElementWithJs(calculate);
+			WebUI.sleep(0.4);
+		} catch (Exception exe) {
+			System.out.println("Calculate button not clicked. " + exe.getMessage());
+		}
+		
 		WebUI.scrollToElementAtBottom(step_1_NextB);
-		WebUI.sleep(0.2);
+		WebUI.sleep(0.5);
 		WebUI.moveToElement(step_1_NextB);
 		WebUI.clickElement(step_1_NextB);
+		WebUI.sleep(2.5);
 		
+		WebUI.scrollToElementAtBottom(step_2_NextB);
 		WebUI.sleep(0.5);
-		WebUI.scrollToElementAtBottom(NextStep2);
-		WebUI.sleep(0.2);
-		WebUI.moveToElement(NextStep2);
-		WebUI.clickElement(NextStep2);
-		WebUI.sleep(1);
+		WebUI.moveToElement(step_2_NextB);
+		WebUI.clickElement(step_2_NextB);
+		WebUI.sleep(3);
 		
 //=======================================================================================================
 		WebUI.sleep(1);
